f91f198d215abf2c362bc9ff5f548be11b4fe53d
Maciej Kobus <maciej.kobus@ez.no>
2023-11-21 09:05:12 +0100

    Added EmailRecipient/SmsRecipient checks to SymfonyRecipientAdapter

--- src/contracts/Value/Recipent/SymfonyRecipientAdapter.php
+++ src/contracts/Value/Recipent/SymfonyRecipientAdapter.php
@@ -9,28 +9,53 @@ declare(strict_types=1);
 namespace Ibexa\Contracts\Notifications\Value\Recipent;
 
 use Ibexa\Contracts\Notifications\Value\RecipientInterface;
-use Symfony\Component\Notifier\Recipient\Recipient;
+use Ibexa\Core\Base\Exceptions\BadStateException;
+use Symfony\Component\Notifier\Recipient\EmailRecipientInterface;
+use Symfony\Component\Notifier\Recipient\RecipientInterface as SymfonyRecipentInterface;
+use Symfony\Component\Notifier\Recipient\SmsRecipientInterface;
 
 final class SymfonyRecipientAdapter implements RecipientInterface
 {
-    private Recipient $recipient;
+    private SymfonyRecipentInterface $recipient;
 
-    public function __construct(Recipient $recipient)
+    public function __construct(SymfonyRecipentInterface $recipient)
     {
         $this->recipient = $recipient;
     }
 
     public function getMail(): string
     {
+        if (!$this->recipient instanceof EmailRecipientInterface) {
+            throw new BadStateException(
+                '$recipient',
+                sprintf(
+                    'Internal %s instance does not implement %s',
+                    SymfonyRecipentInterface::class,
+                    EmailRecipientInterface::class,
+                ),
+            );
+        }
+
         return $this->recipient->getEmail();
     }
 
     public function getPhone(): string
     {
+        if (!$this->recipient instanceof SmsRecipientInterface) {
+            throw new BadStateException(
+                '$recipient',
+                sprintf(
+                    'Internal %s instance does not implement %s',
+                    SymfonyRecipentInterface::class,
+                    SmsRecipientInterface::class,
+                ),
+            );
+        }
+
         return $this->recipient->getPhone();
     }
 
-    public function getRecipient(): Recipient
+    public function getRecipient(): SymfonyRecipentInterface
     {
         return $this->recipient;
     }
