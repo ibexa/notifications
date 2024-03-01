1869af315854f1a520d7da17a58a87eacb8048d7
Maciej Kobus <webhdx@users.noreply.github.com>
2024-03-01 11:16:07 +0100

    IBX-7867: Added unit test coverage (#5)

---
+++ tests/lib/Mapper/RecipientMapperTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Notifications\Mapper;
+
+use Ibexa\Contracts\Notifications\Value\Recipent\SymfonyRecipientAdapter;
+use Ibexa\Contracts\Notifications\Value\RecipientInterface;
+use Ibexa\Core\Base\Exceptions\InvalidArgumentException;
+use Ibexa\Notifications\Mapper\RecipientMapper;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Notifier\Recipient\Recipient;
+
+final class RecipientMapperTest extends TestCase
+{
+    private RecipientMapper $mapper;
+
+    public function setUp(): void
+    {
+        $this->mapper = new RecipientMapper();
+    }
+
+    public function testMapToSymfonyRecipient(): void
+    {
+        $recipient = $this->createMock(Recipient::class);
+
+        $symfonyRecipient = $this->mapper->mapToSymfonyRecipient(
+            new SymfonyRecipientAdapter($recipient)
+        );
+
+        $this->assertSame($recipient, $symfonyRecipient);
+    }
+
+    public function testMapToSymfonyRecipientThrowsExceptionOnInvalidRecipient(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $this->mapper->mapToSymfonyRecipient(
+            $this->createMock(RecipientInterface::class)
+        );
+    }
+}
